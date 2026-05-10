d6096872e6a0f19cc4511f2d6377470087398f34
OmairAttili <omairattili858@gmail.com>
2026-05-10 12:07:16 +0300

    Create categories section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-const getCategories = async()=>{
-    const response  = await axios.get(`https://dummyjson.com/products/category-list`);
-    console.log(response);
+const getcategories = async()=>{
+    const response = await axios.get(`https://dummyjson.com/products/category-list`);
+    const categories = response.data;
+    const displayCategories = categories.map((c)=>{
+       return `<a href="./Categorie.html?${c}" class="bg-amber-400 py-6 px-3 text-center rounded-full font-Manrope font-semibold hover:bg-amber-400/70 hover:scale-95 transition-all"><h2>${c}</h2></a>`;
+    }).join("")
+    document.querySelector(".categories").innerHTML= displayCategories;
 }
-getCategories();
+getcategories();
