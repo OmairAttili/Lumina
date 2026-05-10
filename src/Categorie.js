1ae737c88dfcb130ac087f04e229e6cd3a449613
OmairAttili <omairattili858@gmail.com>
2026-05-10 22:37:41 +0300

    Create Category page

---
+++ src/Categorie.js
@@ -0,0 +1,17 @@
+const loc = location.search.split('?')[1];
+document.querySelector(".categoryName").textContent=loc;
+const getProd = async()=>{
+    const response = await axios.get(`https://dummyjson.com/products/category/${loc}`)
+    const products = response.data.products;
+    const getProducts = products.map((p)=>{
+        return `<a href="./Products.html?${p.id}" class="item shadow-md py-4 hover:border transition-all duration-100">
+                    <img src="${p.thumbnail}" alt="Product"/>
+                    <div class="flex flex-col gap-2 items-center justify-center">
+                        <h2>${p.title}</h2>
+                        <span>$${p.price}</span>
+                    </div>
+                </a>`
+    }).join("")
+    document.querySelector(".Items").innerHTML=getProducts;
+}
+getProd();
